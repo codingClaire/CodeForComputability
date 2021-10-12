fce29f91362bbab5e898d8cc14728a7f27949614
codingClaire <codingClaire@github.com>
2021-10-12 11:20:13 +0800

    basic complementation of turing machine

--- turingMachine.py
+++ turingMachine.py
@@ -1,21 +1,21 @@
 import numpy as np
+from graphviz import Digraph
 
-
-class TuringMachine:
-    def __init__(self, states, start_state, end_states, file_name,stop_step):
+class turingMachine:
+    def __init__(self, states, start_state, end_states, file_name):
         self.states = states
         self.states_num = len(states)
         self.start_state = start_state
         self.end_states = end_states
-        self.stop_step = stop_step  # 防止无法停机的最大步数
         self.next_state = np.zeros(
             (self.states_num, self.states_num), dtype=int)
         self.next_act = np.zeros((self.states_num, self.states_num), dtype=int)
-        f = open(file_name, "r", encoding='utf-8')
-        self.linedata = f.readlines() # 图灵机四元组
+        self.file_name=file_name
         self.get_machine_quadruples()
 
     def get_machine_quadruples(self):
+        f = open(self.file_name, "r", encoding='utf-8')
+        self.linedata = f.readlines() # 图灵机四元组
         act2num = {"L": 0, "R": 1, "0": 2, "1": 3}  # 四种操作
         for line in self.linedata:
             line_data = line.split(" ")
@@ -23,16 +23,29 @@ class TuringMachine:
                 line_data[1]), line_data[2], int(line_data[3])
             self.next_state[from_state][scan_symbol] = to_state
             self.next_act[from_state][scan_symbol] = act2num[act]
+    
+    def get_machine_picture(self):
+        save_name=self.file_name.split(".")[0]
+        print(save_name)
+        tm_graph = Digraph("G",format="png")
+        for state in self.states:
+            if state in self.end_states:
+                tm_graph.node(str(state),label=str(state),shape="doublecircle")
+            else:
+                tm_graph.node(str(state), label=str(state))
+        for line in self.linedata:
+            line_data = line.split(" ")
+            tm_graph.edge(str(line_data[0]),str(line_data[3]),label=str(line_data[1])+":"+str(line_data[2]))
+        tm_graph.render(save_name, view=False)
 
 
-    def scan_tape(self, tape):
-        current_state = start_state
-        current_index = 0
+    def scan_tape(self, stop_step,tape,current_index=0):
+        current_state = self.start_state
         input_conf = tape[0:current_index+1] + \
             "-["+str(current_state)+"]-"+tape[current_index+1:]
         print(input_conf)
         n = 0
-        while(current_state not in end_states and n < self.stop_step):
+        while(current_state not in self.end_states and n < stop_step):
             n += 1
             print("step: ", n)
             scan_symbol = int(tape[current_index])
@@ -61,16 +74,39 @@ class TuringMachine:
         if(current_state not in self.end_states):
             print("the stop number is too small to complete the caculation!")
             print("Please reset the stop number")
+    
+    def test_tm(self):
+        print("==Test for ",self.__class__.__name__, "==")
+        while(1):
+            tape = input("input the current tape(input exit to exit):")
+            if tape=='exit':
+                break
+            current_index=input("input the current index of the scanning head:")
+            stop_step=input("input the largest time to act(in case no halt):")
+            self.scan_tape(int(stop_step),str(tape),int(current_index))
+
+
+class minValueTuringMachine(turingMachine):
+    def __init__(self):
+        states=[i for i in range(1, 32)]
+        end_states=[26, 25, 20, 16, 28, 31]
+        start_state=1
+        file_name="min_fa.txt"
+        super(minValueTuringMachine,self).__init__(states,start_state,end_states,file_name)
+
+class blankNoHaltTuringMachine(turingMachine):
+    def __init__(self):
+        states = [i for i in range(1, 24)]
+        end_states = [18,23]
+        start_state = 1
+        file_name="1halt_fa.txt"
+        super(blankNoHaltTuringMachine,self).__init__(states,start_state,end_states,file_name)
 
 if __name__ == '__main__':
-    states = [i for i in range(1, 32)]
-    end_states = [26, 25, 20, 16, 28, 31]
-    start_state = 1
-    file_name = "./min_fa.txt"
-    stop_step=500
-    while(1):
-        stop_step=input("input the largest time to act(in case no halt):")
-        tape = input("input the current tape:")
-        stop_step,tape=int(stop_step),str(tape)
-        tm_for_min = TuringMachine(states, start_state,end_states,file_name,stop_step)
-        tm_for_min.scan_tape(tape)
+    tm1=minValueTuringMachine()
+    tm1.get_machine_picture()
+    #tm1.test_tm()
+
+    tm2 = blankNoHaltTuringMachine()
+    tm2.get_machine_picture()
+    #tm2.test_tm()
